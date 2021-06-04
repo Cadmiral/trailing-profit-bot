33129f82963f4dc37416d2a072c33a3a02abf742
iMac <user@iMac.lan>
2021-06-03 23:03:18 -0700

    bugfix

--- order.py
+++ order.py
@@ -179,9 +179,12 @@ class OrderMgr:
         orderType = data["type"]
         symbol = data["symbol"]
         side = data["side"]
-        price = float("{:.3f}".format(data["price"]))
-        takeProfit = float("{:.3f}".format(data["take_profit"]))
-        stopLoss = float("{:.3f}".format(data["stop_loss"]))
+        price = float(data["price"])
+        price = "{:.3f}".format(price)
+        takeProfit = float(data["take_profit"])
+        takeProfit = "{:.3f}".format(takeProfit)
+        stopLoss = float(data["stop_loss"])
+        stopLoss = "{:.3f}".format(stopLoss)
         percentageVal = float(data["percentage"])
         strategy = data["strategy"]
 
