624f30c616f20835bab8262476f788fbf8664af3
iMac <user@iMac.lan>
2021-06-06 00:41:56 -0700

    bug

--- order.py
+++ order.py
@@ -361,11 +361,11 @@ class OrderMgr:
                 break
 
             if take_profit_order_status == "FILLED":
+                self.log.info(message)
+                util.sendTelegram(message)
                 profitPrice = float(take_profit_get_order["avgPrice"])
                 profit = (price - profitPrice) * float(take_profit_quantity)
                 message = "TP{0} Profit: ${1:.2f}, symbol: {2}".format(iteration, profit, symbol)
-                self.log.info(message)
-                util.sendTelegram(message)
                 atr_multiplier = 0.5
 
         #         #Create Stop Loss Order
@@ -459,12 +459,12 @@ class OrderMgr:
                 break
 
             if take_profit_order_status == "FILLED":
+                profitPrice = float(take_profit_get_order["avgPrice"])
+                profit = (price + profitPrice) * float(take_profit_quantity)
                 message = "TP{0} Profit: ${1:.2f}, symbol: {2}".format(iteration, profit, symbol)
                 self.log.info(message)
                 util.sendTelegram(message)
 
-                profitPrice = float(take_profit_get_order["avgPrice"])
-                profit = (price - profitPrice) * float(take_profit_quantity)
                 atr_multiplier = 0.5
 
         #         #Create Stop Loss Order
